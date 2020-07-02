0e35e982acffdf88480bbd5f66d8d831ca4997d9
Tapol-Phophum <e27ata@gmail.com>
2020-07-02 21:04:16 +0700

    First Commit
    New issued SortedList

---
+++ SortingTest1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SortingTest1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SortedList sortedList;
+        public MainWindow()
+        {
+            InitializeComponent();
+            sortedList = new SortedList();
+        }
+
+        private void addBtn_Click(object sender, RoutedEventArgs e)
+        {
+            sortedList.Add(int.Parse(keytxt.Text), Valuetxt.Text);
+            ICollection collection = sortedList.Keys;
+            string valueKey = "";
+            foreach (int key in collection)
+            {
+                valueKey = valueKey + "\n" + key + " : " + sortedList[key].ToString();
+            }
+            MessageBox.Show("Count : " + collection.Count.ToString() + "\n"+"Data at below " + valueKey);
+            //MessageBox.Show("Count = : " + collection.Count + valueKey + " ");
+        }
+    }
+}
